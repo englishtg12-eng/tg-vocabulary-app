1d9d07d5cd2c2dcbbb053ec1a2ad7a9547b8ad71
englishtg12-eng <english.tg.12@gmail.com>
2026-09-14 14:57:45 +0900

    Add student self-signup (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v22';
+const CACHE='tg-voca-v23';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
