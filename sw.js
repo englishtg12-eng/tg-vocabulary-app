c203f40ca9f55fd386b19c9e305fd34bb9dfb9f5
englishtg12-eng <english.tg.12@gmail.com>
2026-09-14 15:48:10 +0900

    Simplify app installation (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v24';
+const CACHE='tg-voca-v25';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
