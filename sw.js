f235d8332efab609fbe35f8449a46e59007a05f1
englishtg12-eng <english.tg.12@gmail.com>
2026-09-14 13:23:08 +0900

    Add DAY-based vocabulary exam ranges

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v20';
+const CACHE='tg-voca-v21';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
