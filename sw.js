f3811f6811236060d896ec803cbc6913c6cb2471
englishtg12-eng <english.tg.12@gmail.com>
2026-09-14 14:40:39 +0900

    Add teacher management (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tg-voca-v21';
+const CACHE='tg-voca-v22';
 const ASSETS=['./styles.css','./manifest.webmanifest','./assets/tg-logo.svg'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
